8755ceb1f678e77fdbb16409f14a78e6a50e415f
Yasmineibr1 <yasmine.brahimi321@gmail.com>
2025-07-06 21:47:44 +0200

    add:metrics route config

--- index.js
+++ index.js
@@ -4,10 +4,47 @@ const emailRoutes = require("./src/routes/email");
 
 const app = express();
 const PORT = process.env.PORT || 3005;
+const client = require("prom-client");
 
 app.use(express.json());
-app.use("/api/email", emailRoutes);
+const register = new client.Registry();
+// Crée une métrique de type Counter
+const notifRequestsCounter = new client.Counter({
+  name: "notif_requests_total",
+  help: "Nombre total de requêtes sur le service notif",
+  labelNames: ["method", "route", "status"]
+});
 
+// Enregistre la métrique dans le registre
+register.registerMetric(notifRequestsCounter);
+
+// Collecte les métriques système par défaut
+client.collectDefaultMetrics({ register });
+
+// Middleware pour enregistrer chaque requête
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    notifRequestsCounter.inc({
+      method: req.method,
+      route: req.path,
+      status: res.statusCode
+    });
+  });
+  next();
+});
+app.use("/api/email", emailRoutes);
+app.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", register.contentType);
+  res.send(await register.metrics());
+});
 app.listen(PORT, () => {
   console.log(`📨 Notification service running on port ${PORT}`);
+});
+const metricsApp = express();
+metricsApp.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", register.contentType);
+  res.send(await register.metrics());
+});
+metricsApp.listen(9105, () => {
+  console.log("📊 notif service metrics exposed on http://localhost:9105/metrics");
 });
